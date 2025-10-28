630694e17f1bfd464d47fa06bf667a0d1f974f37
Nacho <nachogutierrez2002@gmail.com>
2025-10-28 14:24:29 +0100

    zustand cart store add products + persist

--- src/app/(shop)/product/[slug]/ui/AddToCart.tsx
+++ src/app/(shop)/product/[slug]/ui/AddToCart.tsx
@@ -1,7 +1,8 @@
 'use client'
 
 import { QuantitySelector, SizeSelector } from "@/components"
-import { Product, Size } from "@/interfaces"
+import { CartProduct, Product, Size } from "@/interfaces"
+import { useCartStore } from "@/store"
 import { useState } from "react"
 
 interface Props {
@@ -9,6 +10,8 @@ interface Props {
 }
 
 export const AddToCart = ({ product }: Props) => {
+
+    const addProductToCart = useCartStore(state => state.addProductToCart)
 
     const [size, setSize] = useState<Size | undefined>()
     const [quantity, setQuantity] = useState(1)
@@ -19,7 +22,22 @@ export const AddToCart = ({ product }: Props) => {
         setPosted(true)
 
         if (!size) return
-        console.log(size, quantity)
+
+        const cartProduct: CartProduct = {
+            id: product.id,
+            slug: product.slug,
+            title: product.title,
+            price: product.price,
+            quantity: quantity,
+            size: size,
+            image: product.images[0]
+        }
+
+        addProductToCart(cartProduct)
+        setPosted(false)
+        setQuantity(1)
+        setSize(undefined)
+
     }
 
 
---
+++ src/store/cart/cart-store.ts
@@ -0,0 +1,49 @@
+import type { CartProduct } from "@/interfaces";
+import { create } from "zustand";
+import { persist } from "zustand/middleware";
+
+interface State {
+    cart: CartProduct[]
+
+    addProductToCart: (product: CartProduct) => void
+}
+
+
+export const useCartStore = create<State>()(
+
+
+    persist(
+        (set, get) => ({
+
+            cart: [],
+
+
+
+            addProductToCart: (product: CartProduct) => {
+                const { cart } = get()
+
+                const productInCart = cart.some(
+                    (item) => item.id === product.id && item.size === product.size
+                )
+
+                if (!productInCart) {
+                    set({ cart: [...cart, product] })
+                    return
+                }
+
+                const updatedCartProduct = cart.map((item) => {
+                    if (item.id === product.id && item.size === product.size) {
+                        return { ...item, quantity: item.quantity + product.quantity }
+                    }
+
+                    return item
+                })
+                set({ cart: updatedCartProduct })
+
+            }
+
+        }), {
+        name: 'shopping-cart'
+    }
+    )
+)
